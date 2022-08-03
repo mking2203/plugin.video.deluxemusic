eba5c765544d75f44fafaed9b7edcff4c1944637
Mark König <mark@koenig-itzehoe.de>
2022-08-03 21:30:49 +0200

    Revert "update addon"
    This reverts commit 3c3433b26e10c117b2982c3de76dfded7addbf2a.

--- addon.py
+++ addon.py
@@ -3,7 +3,7 @@
 
 #  Deluxe Music Addon
 #
-#      Copyright (C) 2020 Mark König
+#      Copyright (C) 2017 Mark König
 #
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -23,11 +23,13 @@
 import os
 import sys
 import urllib
-import urllib.parse as urlparse
+import urlparse
 import time
 
 import re
 import requests
+
+from BeautifulSoup import BeautifulSoup
 
 import json
 
@@ -35,7 +37,6 @@ import xbmc
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
-import xbmcvfs
 
 CommonRootView = 50
 FullWidthList = 51
@@ -77,8 +78,30 @@ class DeluxeMusic(object):
 
         if(url == 'live'):
 
-            url = 'https://deluxemusic.de/mediathek/?v='
-            self.playMedia(url)
+            url = 'https://deluxemusic.tv'
+
+            r = requests.get(url)
+            if r.status_code == requests.codes.ok:
+                result = r.text
+
+                # find webcast
+                s1 = 'webcastId:(.*?),'
+                match = re.search(s1,result, re.DOTALL)
+                if match is not None:
+                    webID = match.group(1).strip()
+                    xbmc.log('- web ID %s' % webID)
+
+                    # find applicationId
+                    s1 = 'applicationId:(.*?),'
+                    match = re.search(s1,result, re.DOTALL)
+                    if match is not None:
+                        appID = match.group(1).strip()
+                        xbmc.log('- app ID %s' % appID)
+
+                        url = 'https://player.cdn.tv1.eu/pservices/player/_x_s-' + appID + '_w-' + webID + '/playlist?playout=hls&noflash=true&theov=2.64.0'
+                        xbmc.log('- url%s' % url)
+
+                        self.playVideo(url)
 
         elif (url == 'audio'):
 
@@ -132,7 +155,9 @@ class DeluxeMusic(object):
             # show mediathek content
 
             thumb =''
-            url = ''
+            title =''
+            idNo=''
+            postNo = ''
 
             # get mediathek channels
             link = 'https://deluxemusic.de/mediathek/'
@@ -143,15 +168,26 @@ class DeluxeMusic(object):
                 xbmc.log('- load mediathek data')
                 result = r.text
 
-                regex = '<a.href="(\?v=.*?)".*?data-id=".*?".*?data-lazy-src="(http.*?)"'
+                regex = 'data-id="(.*?)".*?data-post-id="(.*?)".*?data-lazy-src="(.*?)"(.*?)<\/a>'
                 match = re.findall(regex, result, re.DOTALL)
 
                 for m in match:
 
-                    url = link + m[0]
-                    thumb = m[1]
+                    idNo = m[0]
+                    postNo = m[1]
+                    thumb = m[2]
 
-                    self.addPictureItem('', PATH + '?playmedia=%s' % url, thumb)
+                    data = m[3]
+
+                    # try to read title through logo
+                    regex = 'alt="(.*?)"'
+                    match = re.search(regex, result, re.DOTALL)
+
+                    regex = '<div.class="logo">.*?alt="(.*?)"'
+                    match = re.search(regex, data, re.DOTALL)
+                    if(match is not None):
+                        title = match.group(1)
+                        self.addPictureItem(title, PATH + '?playmedia=%s&post=%s' % (idNo,postNo), thumb)
 
             xbmcplugin.endOfDirectory(HANDLE)
 
@@ -159,91 +195,79 @@ class DeluxeMusic(object):
             # play mediathek files
             self.playVideo(url)
 
+    def showSubtitem(self, url):
+
+        if(DEBUG_PLUGIN):
+            xbmc.log('- show subitem %s -' % url)
+
+        link = 'https://deluxetv-vimp.mivitec.net/getMedium/' + url + ".mp4"
+
+        if(DEBUG_PLUGIN):
+            xbmc.log('- file - ' + link)
+        xbmc.Player().play(link)
+
     def playVideo(self, url):
 
-        # init playlist
-        pl = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-        pl.clear()
-        pl.add(url)
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
+                                  'Referer': 'https://player.cdn.tv1.eu/statics/player/6.5.3/standalone/production/index.html?ext=true&l=e&id=player'
+                    }
 
-        xbmc.Player().play(pl)
-
-    def playMedia(self, url):
-
-        xbmc.log('- play mediathek: ' + url)
-
-        r = requests.get(url)
+        r = requests.get(url, headers = header)
         if r.status_code == requests.codes.ok:
             result = r.text
 
-            # find webID
-            s1 = 'dataId:.*?"(.*?)"'
+            jObj = json.loads(r.text)
+
+            title = jObj['additional']['pl']['entries'][0]['title']
+            #print jObj['additional']['pl']['entries'][0]['video']['src']
+            #print jObj['additional']['pl']['entries'][0]['images'][1]['src']
+
+            playlist = jObj['additional']['pl']['entries'][0]['video']['src']
+
+            # init playlist
+            pl = xbmc.PlayList(1)
+            pl.clear()
+
+            playitem = xbmcgui.ListItem(path=playlist)
+            playitem.setInfo('video', { 'plot': title })
+            playitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
+            playitem.setProperty('inputstream.adaptive.manifest_type', 'hls')
+
+            playitem.setContentLookup(False)
+
+            pl.add(playlist,playitem)
+            xbmc.Player().play(pl)
+
+    def playMedia(self, id, post):
+
+        link = 'https://deluxemusic.de/wp-admin/admin-ajax.php?action=get_teaser_video&teaser_id=%s&post_id=%s'  % (id,post)
+        r = requests.get(link)
+        if r.status_code == requests.codes.ok:
+
+            xbmc.log('- teaser')
+            result = r.text
+
+            # find playlistId:
+            s1 = 'playlistId:(.*?),'
             match = re.search(s1,result, re.DOTALL)
-
-            # fallback Homepage
-            if match is None:
-                s1 = 'window.playerSource.dataId.*?\'(.*?)\''
-                match = re.search(s1,result, re.DOTALL)
-
             if match is not None:
-                webID = match.group(1).strip()
+                playlistId = match.group(1).strip()
+                xbmc.log('- playlistId ID %s' % playlistId)
 
-                # get information
-                url = 'https://playout.3qsdn.com/config/' + webID+ '?key=0&timestamp=0'
+                # find applicationId
+                s1 = 'applicationId:(.*?),'
+                match = re.search(s1,result, re.DOTALL)
+                if match is not None:
+                    appID = match.group(1).strip()
+                    xbmc.log('- app ID %s' % appID)
 
-                r = requests.get(url)
-                if r.status_code == requests.codes.ok:
-                    result = r.text
+                    # https://player.cdn.tv1.eu/pservices/player/_x_s-2749759488/playlist?playout=hls&noflash=true&theov=2.64.0&pl=3159818244
+                    url = 'https://player.cdn.tv1.eu/pservices/player/_x_s-' + appID + '/' + playlistId + '/playlist?playout=hls&noflash=true&theov=2.64.0&pl=' + playlistId
+                    xbmc.log('- url%s' % url)
 
-                    js = json.loads(result)
-                    src = js['sources']
-
-                    xbmc.log('- Titlte: ' + js['title'])
-
-                    # get playlist (first entry)
-                    for s in src:
-                        if src[s] is not None:
-
-                            r = requests.get(src[s])
-                            if r.status_code == requests.codes.ok:
-                                result = r.text
-
-                                lines = result.split('\n');
-
-                                cnt = 0
-                                data = {}
-
-                                # scan for streams / resolution
-                                for l in lines:
-                                    if l.startswith('#EXT-X-STREAM-INF'):
-                                        s1 = 'RESOLUTION=(.*?)x'
-                                        match = re.search(s1,l, re.DOTALL)
-                                        data[match.group(1)] = lines[cnt + 1]
-
-                                    cnt += 1
-
-                                # find highest resolution
-                                resolution = 0
-                                link = ''
-
-                                for i, (k, v) in enumerate(data.items()):
-                                    if int(k) > resolution:
-                                        resolution = int(k)
-                                        link = v
-
-                                xbmc.log('- resolution: %s' % resolution)
-
-                                if len(link) > 0:
-                                    url = os.path.dirname(src[s]) + '/' + link
-
-                                    xbmc.log('- url: %s' % url)
-                                    self.playVideo(url)
-
-                            break
+                    self.playVideo(url)
 
 #### some functions ####
-
-# ----------------------------------------------------------------------------
 
     def addFolderItem(self, title, url):
         list_item = xbmcgui.ListItem(label=title)
@@ -280,8 +304,6 @@ class DeluxeMusic(object):
 
 #### main entry point ####
 
-# ----------------------------------------------------------------------------
-
 if __name__ == '__main__':
 
     ADDON = xbmcaddon.Addon()
@@ -302,10 +324,12 @@ if __name__ == '__main__':
 #try:
     deluxe = DeluxeMusic()
 
-    if ('categories' in PARAMS):
+    if PARAMS.has_key('categories'):
         deluxe.showCategory(PARAMS['categories'][0])
-    elif ('playmedia' in PARAMS):
-        deluxe.playMedia(PARAMS['playmedia'][0])
+    elif PARAMS.has_key('subitem'):
+        deluxe.showSubtitem(PARAMS['subitem'][0])
+    elif PARAMS.has_key('playmedia'):
+        deluxe.playMedia(PARAMS['playmedia'][0], PARAMS['post'][0])
     else:
         deluxe.showSelector()
 #except Exception as e:
